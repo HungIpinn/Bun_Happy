d446a59532cdcda4fabc38a584db7a2a4a1a8fa6
Trần Hùng <tranhung@162858-TNHung-2.local>
2024-05-27 16:15:31 +0700

    get and post basic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia1")
+.get('/post/:id',({params:{id}}) =>{
+  return{id}
+} )
+.post('/post',(body)=>{return body})
+.get('/track/*', ()=>{
+  return "track"
+})
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
