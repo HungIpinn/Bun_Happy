c0174570ad215f92ed5f07ed0c030127d2aef8d5
Trần Hùng <tranhung@162858-TNHung-2.local>
2024-05-27 14:45:59 +0700

    c

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    fetch(req: Request): Response | Promise<Response> {
+        const url = new URL(req.url);
+        if(url.pathname === "/")
+        {
+            return new Response("Home")
+        }
+        if(url.pathname ==="/about")
+        {
+            return new Response("About")
+        }
+        return new Response("404!!!");
+    },
+  
+    // Optional port number - the default value is 3000
+    port: process.env.PORT || 3000,
+  });
